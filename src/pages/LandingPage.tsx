41a5ddbf65ab184d7ca103e6784e34efbc984462
bigfish <tonnymutea273@gmail.com>
2026-08-27 23:23:47 +0300

    feat: increase spacing between buttons and logo to mt-20

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -32,7 +32,7 @@ export const LandingPage = () => {
             </div>
             
             {/* Logo Section */}
-            <div className="mt-12">
+            <div className="mt-20">
               <img 
                 src="/images/laundry/landing/smart-laundry-logo.jpg" 
                 alt="Smart Laundry Logo" 
